b1d7f1b2fddf1ce9b94b55eaed0d4ac36995f3e2
Connor Marsh <149538461+Connoronnno@users.noreply.github.com>
2024-02-16 13:28:52 -0700

    Add files via upload

---
+++ Lib/SWLED.C
@@ -0,0 +1,28 @@
+#include "../../../lib/SWLED.h"
+#include "derivative.h"
+
+void SWL_Init(){
+    PT1AD1 = 0b00000000;
+    DDR1AD1 = 0b11100000;
+    ATD1DIEN = 0b00011111;
+}
+void SWL_ON(SWL_LEDColour led){
+    PT1AD1 |= led;
+}
+
+void SWL_OFF(SWL_LEDColour led){
+    PT1AD1 &= ~led;
+}
+
+void SWL_TOG(SWL_LEDColour led){
+    PT1AD1 ^= led;
+}
+
+unsigned int SWL_PUSHED(SWL_SwitchPos pos){
+    return PT1AD1&pos;
+    }
+
+int SWL_ANY(){
+    return PT1AD1&0b00011111>0;
+    }
+
