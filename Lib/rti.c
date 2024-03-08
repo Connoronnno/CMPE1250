09b0cfdc47a1f97921ddf466dac003e68af3f725
Connor Marsh <149538461+Connoronnno@users.noreply.github.com>
2024-03-08 16:33:14 -0700

    Add files via upload
    v

---
+++ Lib/rti.c
@@ -0,0 +1,24 @@
+#include "rti.h"
+#include "derivative.h"
+
+void RTI_Init(void){
+
+    RTICTL = 0b10010111;
+    CRGINT |= 0b10000000;
+
+
+}
+
+void RTI_Delay_ms(unsigned int ms){
+    
+    CRGINT &= 0b01111111;
+    RTI_Init(); 
+    
+    for(; ms>0; ms--){
+        while(!CRGFLG_RTIF){
+        }
+        CRGFLG = CRGFLG_RTIF_MASK;
+    }
+
+    CRGINT &= 0b01111111;
+}
